c4c75af624e9451ab5fa52a8a12de7fe60f39aa2
Md Khaled Hasan Manna <khmannaict13@gmail.com>
2020-03-30 16:40:17 +0600

    create ui and there outlets and action

--- TextFieldValidation/ViewController.swift
+++ TextFieldValidation/ViewController.swift
@@ -10,10 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UIStackView!
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var phoneTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
+    
+    
+    
+    @IBAction func validationBtnAction(_ sender: Any) {
+        
+        
+    }
+    
 
 
 }
